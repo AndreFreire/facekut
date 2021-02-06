7cb5f5e51e2a9803d7713ccf1669057480c03320
andre.freire <andre.freire@dasa.com.br>
2021-02-06 19:00:19 -0300

    Removing AuditModel's lombok annotation

--- src/main/java/com/facekut/repository/model/AuditModel.java
+++ src/main/java/com/facekut/repository/model/AuditModel.java
@@ -1,19 +1,11 @@
 package com.facekut.repository.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDate;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class AuditModel {
     @Id
     private String id;
