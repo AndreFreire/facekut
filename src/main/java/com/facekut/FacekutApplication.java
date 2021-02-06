4fb0f4fa5fd0a9b9b3f08069e3277c5df2f7d8c9
andre.freire <andre.freire@dasa.com.br>
2021-02-06 18:19:58 -0300

    initial commit

---
+++ src/main/java/com/facekut/FacekutApplication.java
@@ -0,0 +1,13 @@
+package com.facekut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacekutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacekutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facekut/FacekutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facekut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacekutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
